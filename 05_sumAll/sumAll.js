12fa2d617c9c9ca58d82801ae2cc498975283bd5
TomasLanfra <tomilanfranco04@gmail.com>
2024-07-11 19:38:20 -0300

    5 and 6 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a != 'number' || typeof b != 'number' || b < 0 || a < 0) {
+        return 'ERROR';   
+    }
 
+    let start = Math.min(a, b);
+    let end = Math.max(a, b);
+    let sum = 0;
+    
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
