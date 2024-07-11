9a4e9eada5502b9d73746e89f9ce31770f748d88
TomasLanfra <tomilanfranco04@gmail.com>
2024-07-11 19:19:41 -0300

    03 and 04 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let stack = [];
+    let nextCharIndex = word.length;
+    
+    while (nextCharIndex >= 0) {
+        stack.push(word[nextCharIndex]);
+        nextCharIndex--;
+    } 
+    return stack.join('');
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, ...elementsToRemove) {
+    let newArray = [];
+    originalArray.forEach(element => {
+        if (!elementsToRemove.includes(element)) {
+            newArray.push(element);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
