95ec23b03214e25a255985dec02e9f5081fc780c
TomasLanfra <tomilanfranco04@gmail.com>
2024-07-29 23:32:14 -0300

    11 & 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (typeof n == "string") {
+        n = parseInt(n);
+    }
+    if (n < 0) {
+        return "OOPS";
+    }
 
+    if (n === 0) return n;
+    if (n === 1) return 1;
+
+    let firstOperand = 0;
+    let secondOperand = 1;
+    let result;
+    for (let i = 1; i < n; i++) {
+        result = firstOperand + secondOperand;
+        firstOperand = secondOperand;
+        secondOperand = result;
+    }
+    return result;
 };
 
 // Do not edit below this line
