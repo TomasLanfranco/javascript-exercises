e45c69bbd7f276310e9f82d53422941817c60aa7
TomasLanfra <tomilanfranco04@gmail.com>
2024-07-29 23:02:32 -0300

    palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let filteredArray = Array.from(str.toLowerCase().matchAll(/[0-9a-z]?/g));
+    let filteredString = filteredArray.join("");
+    let reversedString = filteredArray.reverse().join("");
+    return filteredString === reversedString;
 };
 
 // Do not edit below this line
