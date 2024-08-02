f0453af41e480950609270c4de37dd79480542ff
TomasLanfra <tomilanfranco04@gmail.com>
2024-08-01 23:14:40 -0300

    12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+    
+    return peopleArray
+    .reduce(((oldestPerson, currentPerson) => comparePerson(oldestPerson, currentPerson)));
 };
+
+const comparePerson = function(personA, personB) {
+    let ageOfPersonA = getAge(personA);
+    let ageOfPersonB = getAge(personB);
+
+    return ageOfPersonA > ageOfPersonB ? personA : personB;
+}
+
+const getAge = function(person) {
+    if (!person.yearOfDeath) {
+        person.yearOfDeath = new Date().getFullYear();
+    }
+
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
